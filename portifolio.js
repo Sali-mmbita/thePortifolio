23be79e02019fe7a214af60167324bc104f1f46e
Sali-mmbita <othyenosalim@gmail.com>
2025-07-14 08:51:53 +0300

    Addition
    Added some images

---
+++ portifolio.js
@@ -0,0 +1,4 @@
+  // Simple JavaScript for mobile menu toggle (optional)
+        document.querySelector('.menu-toggle').addEventListener('click', function() {
+            document.querySelector('.nav-menu').classList.toggle('active');
+        });
